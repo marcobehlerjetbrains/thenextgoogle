cc5212ab1eaa12b219ec6a1a5f57716a81a13fde
Marco Behler <marco@marcobehler.com>
2020-10-12 13:56:02 +0200

    first commit

---
+++ src/main/java/com/jetbrains/teamcity/thenextgoogle/ThenextgoogleApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.teamcity.thenextgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThenextgoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThenextgoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbrains/teamcity/thenextgoogle/ThenextgoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.teamcity.thenextgoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThenextgoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
