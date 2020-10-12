098257df2feabbb1d21360011aabc43cc3e703ba
Marco Behler <marco@marcobehler.com>
2020-10-12 14:07:33 +0200

    Initial Commit - Without Teamcity

--- src/main/java/com/jetbrains/teamcity/thenextgoogle/TheNextGoogleApplication.java
+++ src/main/java/com/jetbrains/teamcity/thenextgoogle/TheNextGoogleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThenextgoogleApplication {
+public class TheNextGoogleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThenextgoogleApplication.class, args);
+		SpringApplication.run(TheNextGoogleApplication.class, args);
 	}
 
 }
