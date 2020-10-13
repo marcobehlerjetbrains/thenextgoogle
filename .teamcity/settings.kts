705a8d9c00492185c2fcf9ca33baf714da88d244
admin <admin@TeamCity>
2020-10-13 13:39:36 +0200

    TeamCity change in 'TheNextGoogle' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -41,7 +41,7 @@ object Build : BuildType({
 
     steps {
         maven {
-            goals = "clean test"
+            goals = "clean package"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
