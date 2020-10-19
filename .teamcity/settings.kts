73c95f291f67ef91ee18bb35f190d489bb899c0a
Marco Behler <marco@marcobehler.com>
2020-10-19 14:29:48 +0200

    wip

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -43,7 +43,7 @@ project {
                 if (it == chain.buildTypes().last()) {
                 it.triggers {
                     vcs {
-                        watchChangesInDependencies = true
+
                     }
                 }
             }
