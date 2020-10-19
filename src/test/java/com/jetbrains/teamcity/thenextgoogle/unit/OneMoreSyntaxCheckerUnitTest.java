4f8910a639f650446fd36abed549b880da1842e5
Marco Behler <marco@marcobehler.com>
2020-10-19 14:31:42 +0200

    wip

---
+++ src/test/java/com/jetbrains/teamcity/thenextgoogle/unit/OneMoreSyntaxCheckerUnitTest.java
@@ -0,0 +1,13 @@
+package com.jetbrains.teamcity.thenextgoogle.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OneMoreSyntaxCheckerUnitTest {
+
+    @Test
+    public void helloMe() {
+        assertEquals(1,1);
+    }
+}
